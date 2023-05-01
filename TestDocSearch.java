33c46b677e6fb2095745fd40feae9df152003e56
asarup <asarup@ucsd.edu>
2023-05-01 11:36:09 -0700

    Lab Exercises
    Created some files to count/match the contents to certain keys using grep. Added a test case to TestDocSearch.java.

--- TestDocSearch.java
+++ TestDocSearch.java
@@ -7,16 +7,23 @@ import java.io.IOException;
 public class TestDocSearch {
 	@Test 
 	public void testIndex() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/");
-    assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/");
+        assertEquals("There are 1391 total files to search.", h.handleRequest(rootPath));
 	}
 	@Test 
 	public void testSearch() throws URISyntaxException, IOException {
-    Handler h = new Handler("./technical/");
-    URI rootPath = new URI("http://localhost/search?q=Resonance");
-    String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
-    assertEquals(expect, h.handleRequest(rootPath));
+        Handler h = new Handler("./technical/");
+        URI rootPath = new URI("http://localhost/search?q=Resonance");
+        String expect = "Found 2 paths:\n./technical/biomed/ar615.txt\n./technical/plos/journal.pbio.0020150.txt";
+        assertEquals(expect, h.handleRequest(rootPath));
 	}
+    @Test
+    public void testSearch2() throws URISyntaxException, IOException {
+        Handler h = new Handler("./technical/plos");
+        URI rootpath = new URI("http://localhost/search?q=base%20pair");
+        String expect = "Found 2 paths:\n./technical/plos/journal.pbio.0020190.txt\n./technical/plos/journal.pbio.0020223.txt";
+        assertEquals(expect, h.handleRequest(rootpath));
+    }
 }
 
